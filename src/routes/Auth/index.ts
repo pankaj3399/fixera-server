e138d4d0f04026bb343dfc2f66226ab20263dec5
Your Name <anafariya@gmail.com>
2025-12-18 14:14:27 +0530

    dcfvbg

--- src/routes/Auth/index.ts
+++ src/routes/Auth/index.ts
@@ -7,7 +7,7 @@ const authRouter = Router();
 authRouter.route('/signup').post(SignUp);
 authRouter.route('/login').post(LogIn);
 authRouter.route('/logout').post(LogOut);
-authRouter.route('/me').get(getMe); // No protect - getMe handles auth check internally
+authRouter.route('/me').get(getMe); 
 authRouter.route('/forgot-password').post(ForgotPassword);
 authRouter.route('/reset-password').post(ResetPassword);
 
